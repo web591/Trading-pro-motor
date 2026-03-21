053526950cb989cbfc6c8608a127acdfab5f6ddf
Ajafa-Jafa <web@drifa.com.mx>
2026-03-21 17:31:22 -0600

    Update run_orquestador.bat

--- pythonProject/run_orquestador.bat
+++ pythonProject/run_orquestador.bat
@@ -1,4 +1,20 @@
 @echo off
-cd /d "C:\Users\ajafa\Mi unidad\sublime_Texts_Github\Trading-pro-motor\pythonProject"
-python orquestador_pro.py
+title 🚀 TRADING ENGINE - SISTEMA ACTIVO
+
+:: 1. Abrir Chrome con tus paneles de monitoreo (Unmineable y TradingView)
+echo 🌐 Abriendo paneles visuales...
+start chrome "https://unmineable.com/address/0x22c0f265c5ccc31e336793ee9420207a81ae2680?coin=USDT" "https://es.tradingview.com/chart/0MbLy4wx/"
+
+:: 2. Entramos a la carpeta del proyecto
+cd /d "C:\Github_Proyects\Trading-pro-motor"
+
+echo ============================================
+echo   INICIANDO ORQUESTADOR DESDE DISCO LOCAL C:
+echo ============================================
+
+:: 3. Ejecutamos el orquestador
+".\.venv\Scripts\python.exe" "pythonProject\orquestador_pro_v2.3.py"
+
+echo.
+echo [!] El Orquestador se ha detenido.
 pause
