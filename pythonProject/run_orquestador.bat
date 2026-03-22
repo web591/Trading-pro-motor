da53eaaf2bf145d43cbb41fa01f5021482833b4f
Ajafa-Jafa <web@drifa.com.mx>
2026-03-21 18:02:29 -0600

    Update run_orquestador.bat
    Abre chrome donde se quedo

--- pythonProject/run_orquestador.bat
+++ pythonProject/run_orquestador.bat
@@ -1,10 +1,10 @@
 @echo off
 title 🚀 TRADING ENGINE - SISTEMA ACTIVO
 
-:: 1. Abrir Chrome en el perfil de Trading (Default)
-echo 🌐 Abriendo paneles visuales en tu perfil de Trading...
-:: Usamos la ruta directa al ejecutable y forzamos el perfil Default
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default" "https://unmineable.com" "https://es.tradingview.com"
+:: 1. Despertar Chrome en el perfil de Trading (Default)
+:: Al no poner URLs, Chrome abrirá exactamente como lo cerraste la última vez
+echo 🌐 Restaurando sesión de Chrome (Perfil Trading)...
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Default"
 
 :: 2. Entramos a la carpeta del proyecto
 cd /d "C:\Github_Proyects\Trading-pro-motor"
